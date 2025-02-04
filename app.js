6b490222a9fd39da48e6ce4191592eb9cfbf9601
Yash Bhawsar <yashbhawsar97@gmail.com>
2025-02-04 18:04:37 +0530

    any/unknown

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
+"use strict";
 // let arr: [string, number] = ['Yash', 27]
 // let arr2: [number, string] = [25, "Dev"]
 // Enums -> enumerations
-var UserRoles;
-(function (UserRoles) {
-    UserRoles["ADMIN"] = "admin";
-    UserRoles["GUEST"] = "guest";
-    UserRoles["SUPER_ADMIN"] = "super_admin";
-})(UserRoles || (UserRoles = {}));
-var StatusCodes;
-(function (StatusCodes) {
-    StatusCodes["ABANDONED"] = "abandoned status code is 500";
-    StatusCodes["NOT_FOUND"] = "notfound status code is 404";
-})(StatusCodes || (StatusCodes = {}));
-console.log(StatusCodes.NOT_FOUND);
+// enum UserRoles {
+//     ADMIN = 'admin',
+//     GUEST = 'guest',
+//     SUPER_ADMIN = 'super_admin'
+// }
+// enum StatusCodes {
+//     ABANDONED = "abandoned status code is 500",
+//     NOT_FOUND = 'notfound status code is 404'
+// }
+let a;
+a = 27;
+a = 'Yash';
+if (typeof a === 'string') {
+    a.toUpperCase();
+}
--- app.ts
+++ app.ts
@@ -4,15 +4,21 @@
 
 // Enums -> enumerations
 
-enum UserRoles {
-    ADMIN = 'admin',
-    GUEST = 'guest',
-    SUPER_ADMIN = 'super_admin'
-}
+// enum UserRoles {
+//     ADMIN = 'admin',
+//     GUEST = 'guest',
+//     SUPER_ADMIN = 'super_admin'
+// }
 
-enum StatusCodes {
-    ABANDONED = "abandoned status code is 500",
-    NOT_FOUND = 'notfound status code is 404'
-}
+// enum StatusCodes {
+//     ABANDONED = "abandoned status code is 500",
+//     NOT_FOUND = 'notfound status code is 404'
+// }
 
-console.log(StatusCodes.NOT_FOUND)
+ let a : unknown
+ a = 27
+ a = 'Yash'
+
+ if(typeof a === 'string') {
+    a.toUpperCase()
+ }
