013ee54959617bac19011f4d71d59c094500ba93
Yash Bhawsar <yashbhawsar97@gmail.com>
2025-02-04 17:52:42 +0530

    enums

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-"use strict";
-let arr = ['Yash', 27];
-let arr2 = [25, "Dev"];
+// let arr: [string, number] = ['Yash', 27]
+// let arr2: [number, string] = [25, "Dev"]
+// Enums -> enumerations
+var UserRoles;
+(function (UserRoles) {
+    UserRoles["ADMIN"] = "admin";
+    UserRoles["GUEST"] = "guest";
+    UserRoles["SUPER_ADMIN"] = "super_admin";
+})(UserRoles || (UserRoles = {}));
+var StatusCodes;
+(function (StatusCodes) {
+    StatusCodes["ABANDONED"] = "abandoned status code is 500";
+    StatusCodes["NOT_FOUND"] = "notfound status code is 404";
+})(StatusCodes || (StatusCodes = {}));
+console.log(StatusCodes.NOT_FOUND);
--- app.ts
+++ app.ts
@@ -1,3 +1,18 @@
-let arr: [string, number] = ['Yash', 27]
+// let arr: [string, number] = ['Yash', 27]
 
-let arr2: [number, string] = [25, "Dev"]
+// let arr2: [number, string] = [25, "Dev"]
+
+// Enums -> enumerations
+
+enum UserRoles {
+    ADMIN = 'admin',
+    GUEST = 'guest',
+    SUPER_ADMIN = 'super_admin'
+}
+
+enum StatusCodes {
+    ABANDONED = "abandoned status code is 500",
+    NOT_FOUND = 'notfound status code is 404'
+}
+
+console.log(StatusCodes.NOT_FOUND)
